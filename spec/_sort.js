ca481646009db4423d67fc8ceab0e54194de9016
ccstudent <ccstudent@ccadmins-Mac-mini.local>
2017-07-14 15:33:18 +0900

    Add mergeSort implementation Add mergeSort test

--- spec/_sort.js
+++ spec/_sort.js
@@ -1,5 +1,26 @@
-describe('Your sorting algo', () => {
-  it('should be truthy', () => {
-    expect(true).toBeTruthy();
+describe('mergeSort', () => {
+  it('should be a function', () => {
+    expect(mergeSort instanceof Function).toBe(true);
+  });
+  it('should return an array', () => {
+    expect(mergeSort([7,5]) instanceof Array).toBe(true);
+  });
+  it('should sort an array with repeated elements correctly', () => {
+    expect(mergeSort([99,1,1,1,2,2,2,7,5])).toEqual([1,1,1,2,2,2,5,7,99]);
+  });
+  it('should sort an array with descending order correctly', () => {
+    expect(mergeSort([99,98,91,7,5,3])).toEqual([3,5,7,91,98,99]);
+  });
+  it('should sort array with odd length correctly', () => {
+    expect(mergeSort([88,1,2,7,5])).toEqual([1,2,5,7,88]);
+  });
+  it('should sort array with even length correctly', () => {
+    expect(mergeSort([99,1,65,68,23,64])).toEqual([1,23,64,65,68,99]);
+  });
+  it('should sort array with string elements correctly', () => {
+    expect(mergeSort(['pegah', 'saba', 'kumiko', 'bill', 'mike'])).toEqual(["bill", "kumiko", "mike", "pegah", "saba"]);
+  });
+  it('should sort correctly', () => {
+    expect(mergeSort([7,5])).toEqual([5, 7]);
   });
 });
--- src/sort.js
+++ src/sort.js
@@ -1,1 +1,70 @@
-console.log('sort this!');
+let merge= function(left, right){
+    let merged = []
+    while(left.length && right.length){
+        if(left[0] <= right[0]){
+            merged.push(left.shift());
+        }
+        else{
+            merged.push(right.shift());
+        }
+    }
+    while(left.length){
+       merged.push(left.shift());
+    }
+    while(right.length){
+       merged.push(right.shift());
+    }
+    return merged;
+    // console.log('here');
+    // let leftSize = m - l + 1;
+    // let rightSize = r - m
+    // let leftArr = [];
+    // let rightArr = [];
+    // for(let i=0; i<leftSize; i++){
+    //     leftArr[i] = arr[l + i];
+    // }
+    // for(let i=0; i<rightSize; i++){
+    //     rightArr[i] = arr[l+m+i];
+    // }
+    // let i=0;
+    // let j=0;
+    // let k=l;
+    // while(i<leftSize && j<rightSize){
+    //     if(leftArr[i] <= rightArr[j]){
+    //         arr[k] = leftArr[i];
+    //         i++;
+    //     }
+    //     else{
+    //         arr[k] = rightArr[j];
+    //         j++;
+    //     }
+    //     k++;
+    // }
+    // while( i < leftSize){
+    //     arr[k] = leftArr[i];
+    //     i++;
+    //     k++;
+    // }
+    // while(j < rightSize){
+    //     arr[k] = rightArr[j];
+    //     j++;
+    //     k++;
+    // }
+}
+
+let mergeSort = function(arr){
+    if(arr.length < 2){
+        return arr;
+    }
+    let mid = arr.length / 2;
+    let left = arr.slice(0, mid);
+    let right = arr.slice(mid, arr.length);
+    return merge(mergeSort(left), mergeSort(right));
+    // if(l < r){
+    //     m = l + (r - 1) / 2;
+    //     mergeSort(arr, l, m);
+    //     mergeSort(arr, m+l, r);
+    //     merge(arr, l, m, r);
+    // }
+    //return arr;
+}
